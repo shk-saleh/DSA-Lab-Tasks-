ed4232fe481683110c803f9ac78c5c27c1526cb2
Muhammad Saleh <salehshazli786@gmail.com>
2024-09-19 18:45:58 +0500

    Lab-task-4 added

---
+++ Lab-Task-4/queue#01.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+
+    private:
+
+    int n,rear,front;
+    int* queueArr = new int[n];
+
+
+    public:
+
+    Queue(){
+        n = 5;
+        front = -1;
+        rear = -1;
+    }
+
+    void enque(int value){
+
+        if(rear == n - 1){
+            cout<<"Queue is full!!"<<endl;
+        }
+        else{
+            rear++;
+            queueArr[rear] = value;
+        }
+
+    }
+
+    int deque(){
+
+        if(front != n-1){ 
+           front++;
+           return queueArr[front];
+        }else{
+            cout<<"Queue is over!!";
+        }
+
+        return -1;
+
+    }
+
+    bool isEmpty(){
+        if(front == n-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+};
+
+
+int main(){
+
+    Queue q;
+
+    q.enque(1);
+    q.enque(2);
+    q.enque(3);
+    q.enque(4);
+    q.enque(5);
+    cout<<"NOW : "<<q.deque()<<endl;
+    cout<<"NOW : "<<q.deque()<<endl;
+    cout<<"NOW : "<<q.deque()<<endl;
+    cout<<"NOW : "<<q.deque()<<endl;
+    cout<<"NOW : "<<q.deque()<<endl;
+
+
+}
---
+++ Lab-Task-4/queue#02.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+
+    private:
+
+    string word1,word2,word3,word4,combo;
+    int wordInc = 0;
+    int whitespace;
+
+    public:
+
+
+    void dequeue(string Line){
+
+        for(int i=0; Line[i]!='\0';i++){    // Loop continue to execute until line terminator reached
+
+            if(Line[i] != ' '){      
+
+                if(wordInc == 0){
+                    word1 += Line[i];
+                } 
+                else if(wordInc == 1){
+                    word2 += Line[i];
+                }  
+                else if(wordInc == 2){
+                    word3 += Line[i];
+                }
+                else if(wordInc == 3){
+                    word4 += Line[i];
+                } 
+
+            }else{                          // if program found space then it move to next word
+                wordInc++;
+            }
+
+        }
+
+    }
+
+
+    void displaywords(){
+
+        cout<<"Queue 1 :  "<< word1<<endl;
+        cout<<"Queue 2 :  "<< word2<<endl;
+        cout<<"Queue 3 :  "<< word3<<endl;
+        cout<<"Queue 4 :  "<< word4<<endl;
+
+    }
+
+    
+    void enqueue(){
+
+        combo = word1 + " " + word2 + " " + word3 + " " + word4;
+
+        cout<<"Enqueued String : "<<combo;
+
+    }
+
+
+};
+
+
+int main(){
+
+    Queue q;
+
+    string Line;
+
+    cout<<"Enter a line : ";
+    getline(cin, Line);
+
+    q.dequeue(Line);    // Break the complete line in singlular words
+    q.displaywords();   // display the single words
+    q.enqueue();        // Again enqueue the line
+
+}
